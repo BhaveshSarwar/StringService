7b2f1a0a35ed26ca9bce918703f299e9581e66ca
Bhavesh Sarwar <bsarwar@inspeero.com>
2018-08-03 10:09:12 +0530

    FrameworkAdded

---
+++ ICFramework/ICFramework/ICFramework.h
@@ -0,0 +1,19 @@
+//
+//  ICFramework.h
+//  ICFramework
+//
+//  Created by Bhavesh Sarwar on 03/08/18.
+//  Copyright © 2018 Bhavesh Sarwar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ICFramework.
+FOUNDATION_EXPORT double ICFrameworkVersionNumber;
+
+//! Project version string for ICFramework.
+FOUNDATION_EXPORT const unsigned char ICFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ICFramework/PublicHeader.h>
+
+
---
+++ ICFramework/ICFramework/service.swift
@@ -0,0 +1,16 @@
+//
+//  service.swift
+//  ICFramework
+//
+//  Created by Bhavesh Sarwar on 03/08/18.
+//  Copyright © 2018 Bhavesh Sarwar. All rights reserved.
+//
+
+import UIKit
+
+class service: NSObject {
+    func doSomething(){
+        print("Something has been printed from the service og ICFramework")
+    }
+
+}
